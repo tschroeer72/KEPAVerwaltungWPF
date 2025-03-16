62add9287df81a938ab1962bd419489d72cb8b2b
tschroeer72 <t.schroeer@web.de>
2025-03-16 09:44:21 +0100

    Modul Vordrucke vorbereitet

--- KEPAVerwaltungWPF/ViewModels/StatistikViewModel.cs
+++ KEPAVerwaltungWPF/ViewModels/StatistikViewModel.cs
@@ -1,30 +1,41 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using KEPAVerwaltungWPF.Services;
 
 namespace KEPAVerwaltungWPF.ViewModels;
 
 public partial class StatistikViewModel : BaseViewModel
 {
-    public StatistikViewModel()
+    private readonly DBService _dbService;
+
+    public StatistikViewModel(DBService dbService)
     {
+        _dbService = dbService;
         Titel = "Statistik";
     }
     
     [ObservableProperty] private bool zeitLaufendeMeisterschaft = true;
-    [ObservableProperty] private bool zeitLetzteMeisterschaft = true;
+    [ObservableProperty] private bool zeitLetzteMeisterschaft = false;
     [ObservableProperty] [NotifyPropertyChangedFor(nameof(ZeitBereichReadOnly))] private bool zeitBereich = false;
     [ObservableProperty] private bool zeitGesamt = false;
     
     [ObservableProperty] private bool auswahlNeuner = true;
-    [ObservableProperty] private bool auswahlRatten = true;
-    [ObservableProperty] private bool auswahlPokal = true;
-    [ObservableProperty] private bool auswahlSarg = true;
-    [ObservableProperty] private bool auswahlErgebnisseSpielerSpieler = true;
-    [ObservableProperty] private bool auswahlPlatzierung6TageRennen = true;
-    [ObservableProperty] private bool auswahlBesteMannschaft6TageREnnen = true;
-    [ObservableProperty] private bool auswahlMannschaftMitglied6TageRennen = true;
-    [ObservableProperty] private bool auswahlNeunerkoenigRattenorden = true;
+    [ObservableProperty] private bool auswahlRatten = false;
+    [ObservableProperty] private bool auswahlPokal = false;
+    [ObservableProperty] private bool auswahlSarg = false;
+    [ObservableProperty] private bool auswahlErgebnisseSpielerSpieler = false;
+    [ObservableProperty] private bool auswahlPlatzierung6TageRennen = false;
+    [ObservableProperty] private bool auswahlBesteMannschaft6TageREnnen = false;
+    [ObservableProperty] private bool auswahlMannschaftMitglied6TageRennen = false;
+    [ObservableProperty] private bool auswahlNeunerkoenigRattenorden = false;
     
     public bool ZeitBereichReadOnly => ZeitBereich;
     [ObservableProperty] DateTime? zeitBereichVon = null;
     [ObservableProperty] DateTime? zeitBereichBis = null;
+
+    [RelayCommand]
+    public void Vorschau()
+    {
+        
+    }
 }
--- KEPAVerwaltungWPF/ViewModels/VordruckeViewModel.cs
+++ KEPAVerwaltungWPF/ViewModels/VordruckeViewModel.cs
@@ -1,9 +1,34 @@
-﻿namespace KEPAVerwaltungWPF.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using KEPAVerwaltungWPF.Services;
+
+namespace KEPAVerwaltungWPF.ViewModels;
 
 public partial class VordruckeViewModel : BaseViewModel
 {
-    public VordruckeViewModel()
+    private readonly DBService _dbService;
+
+    public VordruckeViewModel(DBService dbService)
     {
-        Titel = "Vordrucke";
+        _dbService = dbService;
+        Titel = "Listen und Vordrucke";
+    }
+
+    [ObservableProperty] private bool auswahlAktiveMitglieder = true;
+    [ObservableProperty] private bool auswahlAlleMitglieder = false;
+    
+    [ObservableProperty] private bool auswahl6TageRennen = false;
+    [ObservableProperty] private bool auswahlKombimeisterschaft = false;
+    [ObservableProperty] private bool auswahlMeisterschaft = false;
+    [ObservableProperty] private bool auswahlBlitztunier = false;
+    [ObservableProperty] private bool auswahlWeihnachtsbaum = false;
+    
+    [ObservableProperty] private bool auswahlSpielverluste = false;
+    [ObservableProperty] private bool auswahlAbrechnung = false;
+    
+    [RelayCommand]
+    public void Vorschau()
+    {
+        
     }
 }
